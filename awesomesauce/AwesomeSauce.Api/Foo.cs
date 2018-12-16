4c784cdf6c7413522e6323104b557e2bcc60d856
Rafael Duarte <rafael.dgs.1993@gmail.com>
2018-12-16 14:14:43 -0200

    add methods map and mapWhen

--- awesomesauce/AwesomeSauce.Api/Foo.cs
+++ awesomesauce/AwesomeSauce.Api/Foo.cs
@@ -11,6 +11,25 @@ namespace AwesomeSauce.Api{
 
         //required
         public void Configure(IApplicationBuilder app, IComponent component){
+            app.Use(async (context,next) => {
+                if(context.Request.Path == "/foo"){
+                    await context.Response.WriteAsync($"Welcome to Foo");
+                }
+                else{
+                    await next();
+                }
+            });
+            app.Use(async (context,next) => {
+                if(context.Request.Path == "/bar"){
+                    await context.Response.WriteAsync($"Welcome to Bar");
+                }
+                else{
+                    await next();
+                }
+            });
+            app.Run(async(context) => 
+                await context.Response.WriteAsync($"Welcome to default"));
+                
             app.Run(async (context)=>
                 await context.Response.WriteAsync($"Name is {component.Name}"));
         }
--- awesomesauce/AwesomeSauce.Api/Startup.cs
+++ awesomesauce/AwesomeSauce.Api/Startup.cs
@@ -32,25 +32,20 @@ namespace AwesomeSauce.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.Use(async (context,next) => {
-                if(context.Request.Path == "/foo"){
-                    await context.Response.WriteAsync($"Welcome to Foo");
-                }
-                else{
-                    await next();
-                }
-            });
-            app.Use(async (context,next) => {
-                if(context.Request.Path == "/bar"){
-                    await context.Response.WriteAsync($"Welcome to Bar");
-                }
-                else{
-                    await next();
-                }
-            });
-            app.Run(async(context) => 
-                await context.Response.WriteAsync($"Welcome to default"));
-                
+            app.Map("/foo",configuration => 
+                configuration.Use(async (context,next)=>
+                                    await context.Response.WriteAsync($"Welcome to Foo")
+                                )
+                    );          
+            app.MapWhen(
+                context =>
+                    context.Request.Method == "POST" &&
+                    context.Request.Path == "/bar",
+                config =>
+                    config.Use(async (context,next) => 
+                                    await context.Response.WriteAsync("Welcome to POST /bar")
+                                )
+            );      
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
